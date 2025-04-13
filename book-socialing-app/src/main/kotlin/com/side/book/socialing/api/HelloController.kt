4420229f70aa8a63b061ef0ababe23946f482bce
ong <orihehe@gmail.com>
2025-04-13 23:46:33 +0900

    hello

---
+++ book-socialing-app/src/main/kotlin/com/side/book/socialing/api/HelloController.kt
@@ -0,0 +1,13 @@
+package com.side.book.socialing.api
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    fun hello(): String {
+        return "Hello World!"
+    }
+}
