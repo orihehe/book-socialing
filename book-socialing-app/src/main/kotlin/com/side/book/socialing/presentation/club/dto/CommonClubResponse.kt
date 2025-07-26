6157801153e456854c1c198683ae9a46b2d8e417
방주영 <bahngju@gmail.com>
2025-07-26 21:37:36 +0900

    - 클럽 컨트롤러 api 목데이터 생성 - 클럽 dto 생성

---
+++ book-socialing-app/src/main/kotlin/com/side/book/socialing/presentation/club/dto/CommonClubResponse.kt
@@ -0,0 +1,18 @@
+package com.side.book.socialing.presentation.club.dto
+
+import io.swagger.v3.oas.annotations.media.Schema
+import java.time.LocalDateTime
+
+@Schema(description = "일반 클럽 DTO")
+data class CommonClubResponse(
+    @Schema(description = "클럽 ID", example = "1")
+    val id: Long,
+    @Schema(description = "클럽 이름", example = "saisai")
+    val clubName: String,
+    @Schema(description = "클럽 이미지 URL", example = "https://dummyimage.com/600x400/000/fff&text=DummyClub")
+    val clubImageUrl: String,
+    @Schema(description = "클럽 소개", example = "간단한 클럽 소개")
+    val description: String,
+    @Schema(description = "클럽 멤버 수", example = "8")
+    val memberCount: Int,
+)
