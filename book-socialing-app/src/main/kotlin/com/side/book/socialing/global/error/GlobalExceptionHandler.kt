ac9623804e792cb221c8ae075bad3e833df7253f
ong <orihehe@gmail.com>
2025-08-15 23:29:30 +0900

    로깅

--- book-socialing-app/src/main/kotlin/com/side/book/socialing/global/error/GlobalExceptionHandler.kt
+++ book-socialing-app/src/main/kotlin/com/side/book/socialing/global/error/GlobalExceptionHandler.kt
@@ -2,6 +2,7 @@ package com.side.book.socialing.global.error
 
 import com.side.book.socialing.global.error.exception.ResourceNotFoundException
 import com.side.book.socialing.global.security.exception.JwtAuthenticationException
+import com.side.book.socialing.global.utils.log
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -21,6 +22,7 @@ class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException::class)
     fun handleResourceNotFoundException(e: ResourceNotFoundException): ResponseEntity<ErrorResponse> {
+        log.error("Resource not found", e)
         return ResponseEntity
             .status(HttpStatus.NOT_FOUND)
             .body(ErrorResponse(e.message))
@@ -28,6 +30,7 @@ class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException::class)
     fun handleIllegalArgumentException(e: IllegalArgumentException): ResponseEntity<ErrorResponse> {
+        log.error("Illegal argument", e)
         return ResponseEntity
             .status(HttpStatus.BAD_REQUEST)
             .body(ErrorResponse(e.message))
