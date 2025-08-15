6e32a14bdee4784e1c00b72f2fd50ceb39b51b27
juye.lee <juye.lee@navercorp.com>
2025-08-15 23:42:47 +0900

    Add lnb

--- book-socialing-fe/src/App.tsx
+++ book-socialing-fe/src/App.tsx
@@ -1,12 +1,14 @@
 // App.tsx
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 import NoteView from './pages/note'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<NoteView />} />
+      <Route path="/" element={<Navigate to="/note" replace />} />
+      <Route path="/note" element={<NoteView />} />
+      <Route path="/club" element={<NoteView />} />
       {/* <Route path="/about" element={<AboutPage />} /> */}
     </Routes>
   )
