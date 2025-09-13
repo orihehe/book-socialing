91cd7cf44b990bf7691622d0720f658e83dc5434
bahngju <bahngju@gmail.com>
2025-09-13 22:07:17 +0900

    누락 코드 커밋

--- book-socialing-app/src/main/kotlin/com/side/book/socialing/domain/chat/service/ChatNoteEventListener.kt
+++ book-socialing-app/src/main/kotlin/com/side/book/socialing/domain/chat/service/ChatNoteEventListener.kt
@@ -4,13 +4,17 @@ import com.side.book.socialing.domain.note.event.NoteCreatedEvent
 import com.side.book.socialing.domain.note.event.NoteJoinedEvent
 import com.side.book.socialing.global.utils.log
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Propagation
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.transaction.event.TransactionPhase
 import org.springframework.transaction.event.TransactionalEventListener
 
 @Component
 class ChatNoteEventListener(
     private val chatRoomService: ChatRoomService
 ) {
-    @TransactionalEventListener
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     fun handleNoteCreatedEvent(event: NoteCreatedEvent) {
         try {
             chatRoomService.createChatRoomForNote(
