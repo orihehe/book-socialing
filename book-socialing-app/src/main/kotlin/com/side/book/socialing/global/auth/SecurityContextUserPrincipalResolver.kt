014f39a773c444d30b4c54f9da139aff014b7cdd
ong <orihehe@gmail.com>
2025-09-13 18:22:50 +0900

    exception 분리

--- book-socialing-app/src/main/kotlin/com/side/book/socialing/global/auth/SecurityContextUserPrincipalResolver.kt
+++ book-socialing-app/src/main/kotlin/com/side/book/socialing/global/auth/SecurityContextUserPrincipalResolver.kt
@@ -1,5 +1,6 @@
 package com.side.book.socialing.global.auth
 
+import com.side.book.socialing.global.error.exception.UserNotAuthenticatedException
 import com.side.book.socialing.global.security.principal.UserPrincipal
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.security.core.context.SecurityContextHolder
@@ -17,6 +18,6 @@ class SecurityContextUserPrincipalResolver : UserPrincipalResolver {
         }
 
         // TODO: 추후 인증 안된 사용자에 대한 예외 처리 필요
-        throw IllegalStateException("User not authenticated")
+        throw UserNotAuthenticatedException("User not authenticated")
     }
 }
