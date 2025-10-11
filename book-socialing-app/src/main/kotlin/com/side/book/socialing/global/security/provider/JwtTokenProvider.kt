94eafd1b4588283f69d7be076dd4fc41a5908aeb
ong <orihehe@gmail.com>
2025-10-11 13:45:23 +0900

    jwt log

--- book-socialing-app/src/main/kotlin/com/side/book/socialing/global/security/provider/JwtTokenProvider.kt
+++ book-socialing-app/src/main/kotlin/com/side/book/socialing/global/security/provider/JwtTokenProvider.kt
@@ -1,8 +1,8 @@
 package com.side.book.socialing.global.security.provider
 
+import com.side.book.socialing.global.utils.log
 import io.jsonwebtoken.*
 import io.jsonwebtoken.security.Keys
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.nio.charset.StandardCharsets
@@ -19,7 +19,6 @@ class JwtTokenProvider(
     private val secretKey: Key = Keys.hmacShaKeyFor(secret.toByteArray(StandardCharsets.UTF_8))
     private val accessTokenValidityInMilliseconds: Long = accessTokenValidity * 1000
     private val refreshTokenValidityInMilliseconds: Long = refreshTokenValidity * 1000
-    val logger = LoggerFactory.getLogger(this::class.java)
 
     fun createAccessToken(userEmail: String): String {
         return createToken(userEmail, accessTokenValidityInMilliseconds)
@@ -50,17 +49,18 @@ class JwtTokenProvider(
     }
 
     fun validateToken(token: String): Boolean {
+        log.info("validateToken 호출됨")
         try {
             getClaims(token)
             return true
         } catch (e: Exception) {
-            logger.error("jwt error", e)
+            log.error("jwt error", e)
             when (e) {
-                is io.jsonwebtoken.security.SecurityException, is MalformedJwtException -> logger.error("Invalid JWT signature.", e)
-                is ExpiredJwtException -> logger.error("Expired JWT token.", e)
-                is UnsupportedJwtException -> logger.error("Unsupported JWT token.", e)
-                is IllegalArgumentException -> logger.error("JWT token is invalid.", e)
-                else -> logger.error("An unknown JWT error occurred.", e)
+                is io.jsonwebtoken.security.SecurityException, is MalformedJwtException -> log.error("Invalid JWT signature.", e)
+                is ExpiredJwtException -> log.error("Expired JWT token.", e)
+                is UnsupportedJwtException -> log.error("Unsupported JWT token.", e)
+                is IllegalArgumentException -> log.error("JWT token is invalid.", e)
+                else -> log.error("An unknown JWT error occurred.", e)
             }
         }
         return false
