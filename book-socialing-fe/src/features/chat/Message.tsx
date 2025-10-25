afa265c5106cf8bed68bd83deafeaaff1efadec2
leejuye <30999815+leejuye@users.noreply.github.com>
2025-10-25 23:50:37 +0900

    유저맵 추가

--- book-socialing-fe/src/features/chat/Message.tsx
+++ book-socialing-fe/src/features/chat/Message.tsx
@@ -2,6 +2,7 @@ import dayjs from 'dayjs'
 import { UserRound } from 'lucide-react'
 
 import { ChatMessageResponse, MessageType } from '@/types/chat'
+import { User } from '@/types/user'
 
 import { MESSAGE_TYPE_LABELS } from './const'
 
@@ -17,9 +18,10 @@ import { MESSAGE_TYPE_LABELS } from './const'
 
 interface Props extends ChatMessageResponse {
   onUserClick: (userId: number) => void
+  user?: User
 }
 
-export function Message({ onUserClick, content, type, sentAt, userId }: Props) {
+export function Message({ onUserClick, content, type, sentAt, userId, user }: Props) {
   return (
     <div className="flex gap-2">
       <button className="w-8 h-8 rounded-full cursor-pointer" onClick={() => onUserClick(userId)}>
@@ -27,7 +29,7 @@ export function Message({ onUserClick, content, type, sentAt, userId }: Props) {
       </button>
 
       <div className="flex flex-col gap-1 flex-1 min-w-0">
-        <div className="text-sm font-semibold">{userId}</div>
+        <div className="text-sm font-semibold">{user?.nickname || `User ${userId}`}</div>
         <div className="flex gap-2 relative">
           {/* 말풍선 */}
           <div className="bg-[#EEF2F4] text-sm px-4 py-2 rounded-xl text-black whitespace-pre-wrap break-words max-w-[calc(100vw-120px)]">
@@ -42,7 +44,7 @@ export function Message({ onUserClick, content, type, sentAt, userId }: Props) {
             )}
 
             {/* 시간 */}
-            <div className="text-[10px] text-gray-700">{dayjs(sentAt).format('a hh:mm')}</div>
+            <div className="text-[10px] text-gray-700">{dayjs(sentAt).format('HH:mm')}</div>
           </div>
         </div>
       </div>
