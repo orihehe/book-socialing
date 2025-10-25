65ea344acc86785121529e27285c6309a62a67d8
Juye Lee <30999815+leejuye@users.noreply.github.com>
2025-10-25 22:18:02 +0900

    build 수정 (#93)

--- book-socialing-fe/src/features/chat/Message.tsx
+++ book-socialing-fe/src/features/chat/Message.tsx
@@ -19,18 +19,15 @@ interface Props extends ChatMessageResponse {
   onUserClick: (senderNickname: string) => void
 }
 
-export function Message({ onUserClick, senderNickname, content, type, sentAt }: Props) {
+export function Message({ onUserClick, content, type, sentAt }: Props) {
   return (
     <div className="flex gap-2">
-      <button
-        className="w-8 h-8 rounded-full cursor-pointer"
-        onClick={() => onUserClick(senderNickname)}
-      >
+      <button className="w-8 h-8 rounded-full cursor-pointer" onClick={() => onUserClick('juye')}>
         <UserRound className="w-8 h-8 rounded-full" />
       </button>
 
       <div className="flex flex-col gap-1">
-        <div className="text-sm font-semibold">{senderNickname}</div>
+        <div className="text-sm font-semibold">juye</div>
         <div className="flex gap-2 relative w-fit max-w-[80%]">
           {/* 말풍선 */}
           <div className="bg-[#EEF2F4] text-sm px-4 py-2 rounded-xl text-black whitespace-pre-line">
@@ -39,7 +36,9 @@ export function Message({ onUserClick, senderNickname, content, type, sentAt }: 
           <div className="flex flex-col gap-0 w-auto">
             {/* 타입 태그 */}
             {type !== MessageType.GENERAL && (
-              <div className="text-[11px] text-gray-500">[{MESSAGE_TYPE_LABELS[type]}]</div>
+              <div className="text-[11px] text-gray-500">
+                [{MESSAGE_TYPE_LABELS[type as MessageType]}]
+              </div>
             )}
 
             {/* 시간 */}
