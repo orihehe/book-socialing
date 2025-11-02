6b66e303e938cf2463ad7cd583666d6abc3fc326
ong <orihehe@gmail.com>
2025-11-02 21:27:12 +0900

    fallback

--- book-socialing-app/src/main/kotlin/com/side/book/socialing/domain/user/service/NicknameGenerationService.kt
+++ book-socialing-app/src/main/kotlin/com/side/book/socialing/domain/user/service/NicknameGenerationService.kt
@@ -3,7 +3,9 @@ package com.side.book.socialing.domain.user.service
 
 import com.side.book.socialing.domain.user.repository.UserRepository
 import com.side.book.socialing.global.Constants
+import com.side.book.socialing.global.utils.log
 import org.springframework.stereotype.Service
+import java.util.UUID
 import kotlin.random.Random
 
 @Service
@@ -12,12 +14,15 @@ class NicknameGenerationService(
 ) {
 
     fun generate(): String {
-        var nickname: String
-        do {
+        repeat(10) {
             val adjective = Constants.adjectives.random()
             val noun = Constants.nouns.random()
-            nickname = "$adjective $noun ${Random.nextInt(1000, 9999)}"
-        } while (userRepository.existsByNickname(nickname))
-        return nickname
+            val nickname = "$adjective $noun ${Random.nextInt(1000, 9999)}"
+            if (!userRepository.existsByNickname(nickname)) {
+                return nickname
+            }
+        }
+        log.warn("Failed to generate a unique nickname after 10 attempts.")
+        return "user-${UUID.randomUUID()}"
     }
 }
