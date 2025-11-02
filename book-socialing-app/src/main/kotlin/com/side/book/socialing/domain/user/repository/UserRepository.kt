2e07cff66fbb63ff581239d5c1b0337d836e3a55
bahngju <bahngju@gmail.com>
2025-11-02 17:07:21 +0900

    feature/club (#109)
    사용자 정보 조회 api 구현

--- book-socialing-app/src/main/kotlin/com/side/book/socialing/domain/user/repository/UserRepository.kt
+++ book-socialing-app/src/main/kotlin/com/side/book/socialing/domain/user/repository/UserRepository.kt
@@ -2,7 +2,11 @@ package com.side.book.socialing.domain.user.repository
 
 import com.side.book.socialing.domain.user.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface UserRepository : JpaRepository<User, Long> {
     fun findByEmail(email: String): User?
+
+    @Query("SELECT u FROM User u WHERE u.id = :userId AND u.deleted = false")
+    fun findActiveUserByUserId(userId: Long): User?
 }
