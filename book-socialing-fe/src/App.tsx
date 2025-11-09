31cd7403857cb587b99f4b1ed9c027169d33c7fb
leejuye <30999815+leejuye@users.noreply.github.com>
2025-11-09 23:19:39 +0900

    닫힌 노트 추가

--- book-socialing-fe/src/App.tsx
+++ book-socialing-fe/src/App.tsx
@@ -1,5 +1,6 @@
 // App.tsx
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
@@ -25,6 +26,7 @@ const queryClient = new QueryClient()
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <div className="min-h-screen bg-gray-100">
         <div className="max-w-[500px] mx-auto bg-white shadow-lg min-h-screen">
           <Toaster />
