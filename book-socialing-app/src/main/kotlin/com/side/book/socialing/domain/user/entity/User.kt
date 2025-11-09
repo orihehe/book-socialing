c30ef831436a12c1d4bd55ed2abab10dfd9dac57
bahngju <bahngju@gmail.com>
2025-11-10 00:00:33 +0900

    유저 마이페이지 수정 로직 구현

--- book-socialing-app/src/main/kotlin/com/side/book/socialing/domain/user/entity/User.kt
+++ book-socialing-app/src/main/kotlin/com/side/book/socialing/domain/user/entity/User.kt
@@ -2,15 +2,14 @@ package com.side.book.socialing.domain.user.entity
 
 import com.side.book.socialing.domain.common.BaseEntity
 import com.side.book.socialing.domain.user.command.CreateUserCommand
+import com.side.book.socialing.domain.user.command.UpdateUserCommand
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
 import jakarta.persistence.UniqueConstraint
-import org.hibernate.annotations.BatchSize
 import org.hibernate.annotations.SQLRestriction
 
 @Entity
@@ -31,7 +30,7 @@ class User(
     val email: String,
 
     @Column(nullable = false)
-    val nickname: String,
+    var nickname: String,
 
     @Column(name = "description", length = 1000)
     var description: String? = null,
@@ -39,9 +38,8 @@ class User(
     @Column(nullable = false)
     val role: String = "ROLE_USER",
 
-    @BatchSize(size = 100)
-    @OneToMany(mappedBy = "user")
-    var files: List<UserFile> = listOf(),
+    @Column(name = "profile_image_url", length = 500)
+    var profileImageUrl: String? = null,
 
     @Column(name = "deleted", nullable = false)
     var deleted: Boolean = false
@@ -63,5 +61,14 @@ class User(
 
     fun delete(userId: Long) {
         deleted = true
+    }
+
+    fun update(cmd: UpdateUserCommand) {
+        this.nickname = cmd.nickname
+        this.description = cmd.description
+    }
+
+    fun updateFile(imageUrl: String) {
+        this.profileImageUrl = imageUrl
     }
 }
--- book-socialing-app/src/main/resources/db/migration/V2_user_table.sql
+++ book-socialing-app/src/main/resources/db/migration/V2_user_table.sql
@@ -9,6 +9,7 @@ CREATE TABLE users (
     nickname VARCHAR(100),
     description VARCHAR(1000),
     role VARCHAR(50),
+    profile_image_url VARCHAR(500),
 
     deleted TINYINT NOT NULL DEFAULT FALSE,
     created_at DATETIME NOT NULL,
@@ -20,29 +21,4 @@ CREATE TABLE users (
     UNIQUE KEY uq_email (email),
     INDEX idx_users_created_at (created_at),
     INDEX idx_users_modified_at (modified_at)
-);
-
--- user_file 테이블 생성
-CREATE TABLE user_file (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    user_id BIGINT NOT NULL, -- User 테이블의 ID를 참조
-    original_file_name VARCHAR(255) NOT NULL,
-    stored_file_name VARCHAR(255) NOT NULL UNIQUE, -- 서버에 저장될 고유한 파일명
-    file_path VARCHAR(500) NOT NULL, -- 파일 저장 경로 또는 URL
-    file_size BIGINT NOT NULL, -- 파일 크기 (바이트 단위)
-
-    deleted TINYINT NOT NULL DEFAULT FALSE,
-    created_at DATETIME NOT NULL,
-    created_by VARCHAR(255),
-    modified_at DATETIME NOT NULL,
-    modified_by VARCHAR(255),
-
-    -- 외래 키 제약 조건
-    CONSTRAINT fk_user_file_note
-        FOREIGN KEY (user_id) REFERENCES users(id),
-
-    -- 인덱스
-    INDEX idx_user_file_user_id (user_id),
-    INDEX idx_user_file_created_at (created_at),
-    INDEX idx_user_file_modified_at (modified_at)
 );
