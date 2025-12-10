f856e04b84e3189d92e761fe396eba69e6472806
ong <orihehe@gmail.com>
2025-12-10 21:54:40 +0900

    join chat if club member

--- book-socialing-app/src/main/kotlin/com/side/book/socialing/domain/chat/service/ChatMessageService.kt
+++ book-socialing-app/src/main/kotlin/com/side/book/socialing/domain/chat/service/ChatMessageService.kt
@@ -4,10 +4,14 @@ import com.side.book.socialing.domain.chat.command.SaveMessageCommand
 import com.side.book.socialing.domain.chat.dto.ChatMessagesResponse
 import com.side.book.socialing.domain.chat.dto.SavedMessageDto
 import com.side.book.socialing.domain.chat.entity.ChatMessage
+import com.side.book.socialing.domain.chat.entity.ChatRoomParticipant
 import com.side.book.socialing.domain.chat.entity.MessageType
 import com.side.book.socialing.domain.chat.repository.ChatMessageRepository
 import com.side.book.socialing.domain.chat.repository.ChatRoomParticipantRepository
 import com.side.book.socialing.domain.chat.repository.ChatRoomRepository
+import com.side.book.socialing.domain.club.enum.ParticipantStatus
+import com.side.book.socialing.domain.club.repository.ClubParticipantRepository
+import com.side.book.socialing.domain.note.repository.NoteRepository
 import com.side.book.socialing.global.error.exception.ForbiddenException
 import com.side.book.socialing.global.error.exception.ResourceNotFoundException
 import org.springframework.data.domain.PageRequest
@@ -18,7 +22,9 @@ import org.springframework.transaction.annotation.Transactional
 class ChatMessageService(
     private val chatMessageRepository: ChatMessageRepository,
     private val chatRoomRepository: ChatRoomRepository,
-    private val chatRoomParticipantRepository: ChatRoomParticipantRepository
+    private val chatRoomParticipantRepository: ChatRoomParticipantRepository,
+    private val noteRepository: NoteRepository,
+    private val clubParticipantRepository: ClubParticipantRepository
 ) {
 
     @Transactional
@@ -55,8 +61,25 @@ class ChatMessageService(
     ): ChatMessagesResponse {
         val chatRoom = chatRoomRepository.findByNoteId(noteId)
             ?: throw ResourceNotFoundException("ChatRoom not found")
-        chatRoomParticipantRepository.findByChatRoomIdAndUserId(chatRoom.id!!, userId)
-            ?: throw ForbiddenException("User $userId not participate chat room $noteId")
+
+        // TODO: refactor this
+        var chatRoomParticipant = chatRoomParticipantRepository.findByChatRoomIdAndUserId(chatRoom.id!!, userId)
+
+        if (chatRoomParticipant == null) {
+            val note = noteRepository.findByIdAndDeletedFalse(noteId)
+                ?: throw ResourceNotFoundException("Note not found for chat room $noteId")
+
+            val club = note.club
+            if (club != null) {
+                val clubParticipant = clubParticipantRepository.findByClubIdAndUserId(club.id!!, userId)
+                if (clubParticipant != null && clubParticipant.status == ParticipantStatus.JOINED) {
+                    // User is a joined member of the club, add them as a chat room participant
+                    chatRoomParticipant = chatRoomParticipantRepository.save(ChatRoomParticipant.create(chatRoom, userId))
+                }
+            }
+        }
+
+        chatRoomParticipant ?: throw ForbiddenException("User $userId not participate chat room $noteId")
         val roomId = chatRoom.id
 
         val cursor = lastMessageId
